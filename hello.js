7d00cc7b8f2a3524a64a4f12044b6072525778c4
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-03-09 21:34:39 +0530

    Initial commit

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+jQuery(document).ready(function(){
+	jQuery("#submitAjaxButton").click(function(){
+		//alert("YOu clicked on the button");
+		jQuery.ajax({
+			url: Drupal.settings.basePath + 'gethello',
+			data:{
+				name: jQuery('#edit-name').val(),
+				token: mytoken
+			},
+			success:function(data){
+				//data: value returned from server
+				jQuery('#msg-display-area').html(data)
+			}
+		})
+	});
+});
