af6595f31403040a5975995790b9f4dd47155e03
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-03-09 21:47:33 +0530

    Standardized the files

--- hello.js
+++ hello.js
@@ -1,17 +1,17 @@
 
 jQuery(document).ready(function(){
-	jQuery("#submitAjaxButton").click(function(){
-		//alert("YOu clicked on the button");
-		jQuery.ajax({
-			url: Drupal.settings.basePath + 'gethello',
-			data:{
-				name: jQuery('#edit-name').val(),
-				token: mytoken
-			},
-			success:function(data){
+  jQuery("#submitAjaxButton").click(function(){
+    //alert("YOu clicked on the button");
+    jQuery.ajax({
+      url: Drupal.settings.basePath + 'gethello',
+      data:{
+        name: jQuery('#edit-name').val(),
+        token: mytoken
+      },
+      success:function(data){
 				//data: value returned from server
-				jQuery('#msg-display-area').html(data)
-			}
-		})
-	});
+        jQuery('#msg-display-area').html(data)
+      }
+    })  
+  });
 });
